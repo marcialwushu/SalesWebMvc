73fd13cad3b2f29653aa8f6166a9c7f95fa69884
Cleilson Sousa Pereira <cleilson.pereira@fitbank.com.br>
2020-08-28 08:52:58 -0300

    CRUD Scaffolding

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using SalesWebMvc.Data;
 
 namespace SalesWebMvc
 {
@@ -32,6 +34,9 @@ namespace SalesWebMvc
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<SalesWebMvcContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("SalesWebMvcContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
