af049152cb9e3a6dc52c615f072e4356d77b9cf1
Cleilson Sousa Pereira <cleilson.pereira@fitbank.com.br>
2020-08-31 11:48:12 -0300

    Tratamento de exceção para delete (integridade referencial)

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -68,8 +68,18 @@ namespace SalesWebMvc.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Delete(int id)
         {
-            await _sellerService.RemoveAsync(id);
-            return RedirectToAction(nameof(Index));
+            try
+            {
+                await _sellerService.RemoveAsync(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (IntergrityException e)
+            {
+
+                return RedirectToAction(nameof(Error), new { message = e.Message });
+            }
+            
+            
         }
 
         public async Task<IActionResult> Details(int? id)
