79964efc48e0b542dbc5cca4dd0fab3846c10330
Cleilson Sousa Pereira <cleilson.pereira@fitbank.com.br>
2020-09-02 08:51:38 -0300

    MVC Movie added [Model | Controller | View]

--- Vidly/Vidly/Controllers/MoviesController.cs
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Vidly.Models;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
         // GET: Movies
-        public ActionResult Index()
+        public ActionResult Random()
         {
-            return View();
+            var movie = new Movie() { Name = "Shrek!" };
+            return View(movie);
         }
     }
 }
